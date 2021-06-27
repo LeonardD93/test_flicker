e6c6e96c1432e83727799109debaba9deb04ef97
leonard77dam@yahoo.it <leonard77dam@yahoo.it>
2021-06-27 21:43:34 +0200

    flicker model

---
+++ app/Models/Flicker.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Flicker extends Model {
+    protected $table = 'flicker';
+    protected $guarded = [];
+    
+}
+?>
