85acd9e403130990c82b3817e14727e9c3a5e1f0
leonard77dam@yahoo.it <leonard77dam@yahoo.it>
2021-07-01 22:26:01 +0200

    csrf

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,15 @@
 require('./bootstrap');
 
 import Vue from "vue";
+import VueCsrf from 'vue-csrf';
+ 
+
 
 Vue.component('Flicker', require('./components/Flicker.vue').default);
 Vue.component('FlickerDetail', require('./components/FlickerDetail.vue').default);
+Vue.component('FlickerForm', require('./components/FlickerForm.vue').default);
 Vue.component('modal', require('./components/modal.vue').default);
 
 new Vue().$mount("#app");
+Vue.use(VueCsrf);
 
